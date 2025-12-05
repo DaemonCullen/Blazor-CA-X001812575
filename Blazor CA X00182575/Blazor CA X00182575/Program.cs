fefe8f67f6fdaec134d70b93e2735ed6fc4c09d0
Daemon <x00182575@mytudublin.ie>
2025-12-05 12:57:21 +0000

    Blazor CA setup.

---
+++ Blazor CA X00182575/Blazor CA X00182575/Program.cs
@@ -0,0 +1,11 @@
+using Blazor_CA_X00182575;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
