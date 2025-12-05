a316b9d528f8881383cd19a533f041321aa331b5
Daemon <x00182575@mytudublin.ie>
2025-12-05 19:00:57 +0000

    Countries Page added. Sorting added

---
+++ Blazor CA X00182575/Blazor CA X00182575/src/Country.cs
@@ -0,0 +1,23 @@
+﻿namespace CA3_X00182575.Data
+{
+    public class Country
+    {
+        public Name name { get; set; }
+        public List<string> capital { get; set; }
+        public string region { get; set; }
+        public long population { get; set; }
+        public double area { get; set; }
+        public Flags flags { get; set; }
+        public List<double> latlong { get; set; }
+    }
+
+    public class Name
+    {
+        public string common { get; set; }
+    }
+
+    public class Flags
+    {
+        public string png { get; set; }
+    }
+}
